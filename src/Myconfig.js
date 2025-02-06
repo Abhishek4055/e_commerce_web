bffce4775d6dc0274d462e4f6eb5ad519929154c
Abhihsek <abhishekanand.jnv@gmail.com>
2025-02-06 13:45:07 +0530

    .

--- src/Myconfig.js
+++ src/Myconfig.js
@@ -1,3 +1,4 @@
 export const SEARCH_URL = `https://api.searchspring.net/api/search/search.json`;
-export const SITE_ID = process.env.REACT_APP_SITE_ID || "scmq7n";
+// export const SITE_ID = process.env.REACT_APP_SITE_ID || "scmq7n";
+export const SITE_ID = process.env.REACT_APP_SITE_ID;
 export const RESULTS_FORMET = "native";
